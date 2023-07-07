259f75ef9b2f96cde06797a454c1bc50c34c55e2
csakauye <casakauye@gmail.com>
2023-07-06 21:03:59 -0700

    created SQL database and connected SQL Alchemy to postgres

---
+++ ufo_database.sql
@@ -0,0 +1,16 @@
+DROP TABLE ufo;
+
+CREATE TABLE ufo (
+	" " INT,
+	"Country" VARCHAR,
+	"City" VARCHAR, 
+	"State" VARCHAR,
+	"Shape" VARCHAR,
+	"Summary" TEXT,
+	"lat" DECIMAL,
+	"lng" DECIMAL,
+	"Clean Date" DATE
+);
+
+SELECT * FROM ufo
+
