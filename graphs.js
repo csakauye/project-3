449519c7d557951aca2b6ab81e048d8e5f997bda
amberaeschbach <araeschbach@ucdavis.edu>
2023-07-11 19:04:04 -0700

    trends update

---
+++ graphs.js
@@ -0,0 +1,28 @@
+let url = "http://127.0.0.1:5000/api/ufo";
+d3.json(url).then(function(data){console.log(data)})
+
+let lights = 0;
+let circles = 0;
+let triangles = 0;
+let fireballs = 0;
+let others = 0;
+
+let alien = ""
+
+for(let i = 0;i<ufo.length;i++){
+    alien = ufo[i]
+    if (alien.Shape == "Light") lights+=1;
+    else if (alien.Shape == "Circle") circles+=1;
+    else if (alien.Shape == "Triangle") triangles+=1;
+    else if (alien.Shape == "Fireball") fireballs+=1;
+    else others+=1;
+}
+
+let shapes = []
+shapes.push(lights);
+shapes.push(circles);
+shapes.push(triangles);
+shapes.push(fireballs);
+shapes.push(others);
+
+console.log(shapes)
