6432801e9f621d619f8d730eebb7facd76b0a0df
Layla <layla.burgan@gmail.com>
2023-07-11 21:53:57 -0700

    Added barchart and update CSS

--- graphs.js
+++ graphs.js
@@ -26,20 +26,20 @@ d3.json(url).then(function(data){ufo=data
     shapes.push(others);
     
     console.log(shapes)    
-    new Chart(document.getElementById("graph"), {
+    new Chart(document.getElementById("pie"), {
         type: 'pie',
         data: {
           labels: ["Lights", "Circles", "Triangles", "Fireballs", "Others"],
           datasets: [{
             backgroundColor: ["Yellow", "Lime","RoyalBlue","Red","Orchid"],
             data:shapes,
-            color: "#f1f1f1"
+            color: "#0d0d0d"
           }]
         },
         options: {
           plugins: {
             legend: {
-                labels: {color:"#f1f1f1"}
+                labels: {color:"#0d0d0d"}
             }
           },  
           title: {
@@ -47,4 +47,70 @@ d3.json(url).then(function(data){ufo=data
             text: 'Most Popular UFO Shapes'
           }
         }
-    });});
+    });
+  let CA   = 0;	
+  let FL	 = 0;
+  let AZ	 = 0;
+  let WA	 = 0;
+  let NC	 = 0;
+  let NY	 = 0;
+  let CO	 = 0;
+  let PA	 = 0;
+  let OR	 = 0;
+  let OH   = 0;
+
+  for(let i = 0; i < ufo.length;i++){
+    sighting = ufo[i];
+    if(sighting.State == "CA") CA +=1;
+    else if(sighting.State == "FL") FL += 1;
+    else if(sighting.State =="AZ") AZ +=1;
+    else if(sighting.State == "WA") WA +=1;
+    else if(sighting.State == "NC") NC +=1;
+    else if(sighting.State == "NY") NY +=1;
+    else if(sighting.State == "CO") CO +=1;
+    else if(sighting.State == "PA") PA +=1;
+    else if(sighting.State == "OR") OR +=1;
+    else if(sighting.State == "OH") OH +=1;
+  }
+  let states = []
+  states.push(CA)
+  states.push(FL)
+  states.push(AZ)
+  states.push(WA)
+  states.push(NC)
+  states.push(NY)
+  states.push(CO)
+  states.push(PA)
+  states.push(OR)
+  states.push(OH)
+console.log(`State ` + states)
+// Bar chart
+new Chart(document.getElementById("bar"), {
+  type: 'bar',
+  data: {
+    labels: ["CA", "FL", "AZ", "WA", "NC","NY",	"CO",	"PA",	"OR",	"OH"],
+    datasets: [
+      {
+        backgroundColor: ["Orchid","Yellow","Lime","RoyalBlue","Red","MediumSeaGreen","DarkOrange","DarkTurquoise","MediumPurple","SlateGray"],
+        data: states,
+        color: "#f1f1f1"
+      }
+    ]
+  },
+  options: {
+    plugins:{
+      title: {
+        display: false,
+        text: 'Sightings by US State'
+      },
+      legend: {
+        labels: {color:"#f1f1f1"},
+        display: false
+    }}
+    
+    
+}})
+  
+  
+  
+  });
